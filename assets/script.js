bd0f2dd42996606c021a3ed7a1eca2f8eccad3ac
Corey Jones <staaj2c@gmail.com>
2026-02-16 20:09:05 -0600

    Rebuilt site, added team page, added FAQ, removed old layout

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".faq-item").forEach(item => {
-  item.addEventListener("click", () => {
-    const answer = item.querySelector(".faq-answer");
-    answer.style.display =
-      answer.style.display === "block" ? "none" : "block";
-  });
-});
